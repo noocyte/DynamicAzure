a0cb6a04dc1a172535ec2a6ecc7195832ce8d33b
noocyte <jarle.nygaard@outlook.com>
2013-10-20 18:13:15 +0200

    Lots of refactoring

--- DynamicAzure/Controllers/EntityController.cs
+++ DynamicAzure/Controllers/EntityController.cs
@@ -1,8 +1,6 @@
 ﻿using Cyan;
 using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
@@ -11,17 +9,13 @@ namespace DynamicAzure.Controllers
 {
     public class EntityController : ApiController
     {
-
         CyanClient TableClient = new CyanClient("dynamicazure", "NWEBI6mTJihjtv2VqTNV2ImcVAJ6uc1mqz097C2qLINuHdApWvQ4woO/mNGHE4H5pt4ISzCfGTODc9wEUq2Ckw==");
-
 
         // GET api/entity
         public HttpResponseMessage Get(string client, string entity)
         {
-            var tablename = string.Format("{0}{1}", client, entity);
-
-            // get a table reference (this does not make any request)
-            var table = TableClient[tablename];
+            var tablename = VerifyTable(client, entity);
+            var table = GetTable(tablename);
 
             return Request.CreateResponse(HttpStatusCode.OK, table.Query("PartitionKey"));
         }
@@ -29,10 +23,8 @@ namespace DynamicAzure.Controllers
         // GET api/entity/5
         public HttpResponseMessage Get(string client, string entity, string id)
         {
-            var tablename = string.Format("{0}{1}", client, entity);
-
-            // get a table reference (this does not make any request)
-            var table = TableClient[tablename];
+            var tablename = VerifyTable(client, entity);
+            var table = GetTable(tablename);
 
             return Request.CreateResponse(HttpStatusCode.OK, table.Query("PartitionKey", id));
         }
@@ -40,16 +32,41 @@ namespace DynamicAzure.Controllers
         // PUT api/entity/5
         public HttpResponseMessage Put(string id, string client, string entity, dynamic dynamicObj)
         {
-            // make sure the table has been created
-            var tablename = string.Format("{0}{1}", client, entity);
-            TableClient.TryCreateTable(tablename);
-
-            // get a table reference (this does not make any request)
-            var table = TableClient[tablename];
+            var tablename = VerifyTable(client, entity);
+            var table = GetTable(tablename);
 
             InsertObject(id, dynamicObj, table);
+            return Respond(client, entity, dynamicObj, id);
+        }
 
-            string uri = Url.Link("DefaultApi", new { id = id, controller = "Entity", client = client, entity = entity });
+        // POST api/entity
+        public HttpResponseMessage Post(string client, string entity, dynamic dynamicObj)
+        {
+            var tablename = VerifyTable(client, entity);
+            var table = GetTable(tablename);
+
+            var rowKey = Guid.NewGuid().ToString(); // get proper UXID!
+            InsertObject(rowKey, dynamicObj, table);
+
+            return Respond(client, entity, dynamicObj, rowKey);
+        }
+
+        private CyanTable GetTable(string tablename)
+        {
+            var table = TableClient[tablename];
+            return table;
+        }
+
+        private string VerifyTable(string client, string entity)
+        {
+            var tablename = string.Format("{0}{1}", client, entity);
+            TableClient.TryCreateTable(tablename);
+            return tablename;
+        }
+
+        private HttpResponseMessage Respond(string client, string entity, dynamic dynamicObj, string rowKey)
+        {
+            string uri = Url.Link("DefaultApi", new { id = rowKey, controller = "Entity", client = client, entity = entity });
             var response = Request.CreateResponse(HttpStatusCode.Created, dynamicObj as JObject);
             response.Headers.Location = new Uri(uri);
             return response;
@@ -57,7 +74,6 @@ namespace DynamicAzure.Controllers
 
         private static void InsertObject(string id, dynamic dynamicObj, CyanTable table)
         {
-            // insert an entity
             table.Insert(new
             {
                 PartitionKey = "PartitionKey",
@@ -65,31 +81,6 @@ namespace DynamicAzure.Controllers
                 Name = dynamicObj.Name.Value,
                 Age = dynamicObj.Age.Value
             });
-        }
-
-        // POST api/entity
-        public HttpResponseMessage Post(string client, string entity, dynamic dynamicObj)
-        {
-            // make sure the table has been created
-            var tablename = string.Format("{0}{1}", client, entity);
-            TableClient.TryCreateTable(tablename);
-
-            // get a table reference (this does not make any request)
-            var table = TableClient[tablename];
-
-            // insert an entity
-            var rowKey = Guid.NewGuid().ToString(); // get proper UXID!
-            InsertObject(rowKey, dynamicObj, table);
-
-            string uri = Url.Link("DefaultApi", new { id = rowKey, controller = "Entity", client = client, entity = entity });
-            var response = Request.CreateResponse(HttpStatusCode.Created, dynamicObj as JObject);
-            response.Headers.Location = new Uri(uri);
-            return response;
-        }
-
-        // DELETE api/entity/5
-        public void Delete(int id)
-        {
         }
     }
 }
